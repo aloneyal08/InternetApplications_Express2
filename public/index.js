90da0cc938f293a9defe208c59524d479f88d241
YanivPlayZ <yaniv8532@gmail.com>
2024-05-16 16:36:11 +0200

    start of html and form

--- public/index.js
+++ public/index.js
@@ -8,3 +8,21 @@ const getCars = async () => {
     xhr.open("GET", "/cars");
     xhr.send();
 }
+const submit = () => {
+    const xhr = new XMLHttpRequest();
+    xhr.onload = () => {
+        const inputs = document.querySelectorAll(".formPart");
+        console.log(inputs[7].value);
+        const obj = {
+            name: inputs[1].value,
+            model: inputs[3].value,
+            importer: inputs[5].value,
+            color: inputs[7].value,
+            year: inputs[9].value,
+            price: inputs[11].value
+        };
+    }
+    xhr.getResponseHeader("Content-type", "application/json"); 
+    xhr.open("POST", "/cars");
+    xhr.send();
+}
